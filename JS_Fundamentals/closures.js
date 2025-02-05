97c55baf246f285264a6062bb91b32f3de0c8226
Fidel A Hernandez Jr <fidelhernandez_8@hotmail.com>
2025-02-05 00:52:41 -0600

    built the counter app
    practice closures

--- JS_Fundamentals/closures.js
+++ JS_Fundamentals/closures.js
@@ -1,10 +1,22 @@
-function numGenerator() {
-    var num = 1;
-    function checkNumber() {
-        console.log(num);
+// function numGenerator() {
+//     var num = 1;
+//     function checkNumber() {
+//         console.log(num);
+//     }
+//     return checkNumber;
+// }
+
+// const myFunc = numGenerator();
+// myFunc();
+
+function calculate() {
+    var num = 5;
+    function sum() {
+        let result = num + 10;
+        return result;
     }
-    return checkNumber;
+    return sum;
 }
 
-const myFunc = numGenerator();
-console.log(myFunc);
+const myFunc = calculate();
+myFunc;
---
+++ JS_Fundamentals/counter_app.js
@@ -0,0 +1,26 @@
+function createCounter() {
+    let count = 0;
+
+    return {
+        increment: function() {
+            count++;
+        },
+        decrement: function() {
+            count--;
+        },
+        getValue: function() {
+            return count;
+        }
+    };
+}
+
+const counter = createCounter();
+
+counter.increment();
+counter.increment();
+
+console.log(counter.getValue());
+
+counter.decrement();
+
+console.log(counter.getValue());
