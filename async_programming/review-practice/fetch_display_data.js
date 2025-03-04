280ff5bbb748697cc58158c148710148573e3f61
FHernandez08 <fidelhernandez_8@hotmail.com>
2025-03-04 14:29:10 -0600

    redo the async programming practice/review questions

--- async_programming/review-practice/fetch_display_data.js
+++
@@ -1,4 +0,0 @@
-fetch("https://jsonplaceholder.typicode.com/todos/1")
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log('Error:', error))
--- async_programming/review-practice/parallel_api.js
+++
@@ -1,9 +0,0 @@
-const promise1 = fetch("https://jsonplaceholder.typicode.com/todos/1")
-    .then(response => response.json())
-
-const promise2 = fetch("https://jsonplaceholder.typicode.com/todos/2")
-    .then(response => response.json())
-
-Promise.all([promise1, promise2])
-    .then(values => console.log(values))
-    .catch(error => console.log('Error:', error))
