0844abfb5423c1690bbb5de9e744e803e07a42ee
Fidel A Hernandez Jr <fidelhernandez_8@hotmail.com>
2025-03-16 10:37:20 -0500

    working on the functionality

--- Weather App/main.js
+++ Weather App/main.js
@@ -1,1 +1,10 @@
 // functionality for the weather app = make comments at every important part of the code
+const cityInput = document.querySelector('#search-bar');
+const searchBtn = document.querySelector('#search-button');
+
+let weatherApi = "85cc60d3ab42f69355786efe014e8cce";
+
+fetch(weatherApi)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log('Error:', error))
