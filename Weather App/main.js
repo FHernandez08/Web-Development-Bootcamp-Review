161443f3f9d497ba111553182975c3e35aca1daa
FHernandez08 <fidelhernandez_8@hotmail.com>
2025-03-21 10:58:13 -0500

    Getting data from weather API

--- Weather App/main.js
+++ Weather App/main.js
@@ -1,10 +1,23 @@
 // functionality for the weather app = make comments at every important part of the code
-const cityInput = document.querySelector('#search-bar');
 const searchBtn = document.querySelector('#search-button');
 
 let weatherApi = "85cc60d3ab42f69355786efe014e8cce";
 
-fetch(weatherApi)
-    .then(response => response.json())
+searchBtn.addEventListener("click", function() {
+    const cityInput = document.querySelector('#search-bar').value;
+    if (!cityInput){
+        console.log("Please enter a city name.");
+        return;
+    }
+
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(cityInput)}&appid=${weatherApi}&units=imperial`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response did NOT work');
+        }
+        return response.json();
+    })
     .then(data => console.log(data))
-    .catch(error => console.log('Error:', error))
+    .catch(error => console.log(error))
+})
