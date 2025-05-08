7647e47b2b3054408a407ba41d6f442f3d1282ff
FHernandez08 <fidelhernandez_8@hotmail.com>
2025-05-08 12:03:24 -0500

    technical fix - records weren't showing

--- express-api-development/learning/crossfit-wod-api/src/controllers/recordController.js
+++ express-api-development/learning/crossfit-wod-api/src/controllers/recordController.js
@@ -1,8 +1,21 @@
 const recordService = require("../services/recordService");
 
 const getRecordForWorkout = (req, res) => {
+    const {
+        params: { workoutId },
+    } = req;
+
+    if (!workoutId) {
+        res
+            .status(400)
+            .send({
+                status: "FAILED",
+                data: { error: "Parameter ':workoutId' can not be empty"}
+            });
+    }
+
     try {
-        const getRecord = recordService.getRecordForWorkout();
+        const getRecord = recordService.getRecordForWorkout(workoutId);
         res.send({ status: "OK", data: getRecord });
     }
 
