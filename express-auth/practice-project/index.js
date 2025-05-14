5ea4987149a0fba225d7945a6b8f75a62bbcad5e
FHernandez08 <fidelhernandez_8@hotmail.com>
2025-05-14 10:41:30 -0500

    implemented token authentication

--- express-auth/practice-project/index.js
+++ express-auth/practice-project/index.js
@@ -2,9 +2,30 @@ require('dotenv').config();
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authentication'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Token required '});
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.status(403).json({ message: 'Invalid token' });
+
+        req.user = user;
+        next();
+    })
+}
+
 const app = express();
 app.use(express.json());
 
+app.get('/dashboard', authenticateToken, (req, res) => {
+    res.json({ message: `Welcome ${req.user.username}, this is your dashboard!` });
+});
+
+// used to create token after successfully logging in
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
@@ -17,3 +38,7 @@ app.post('/login', (req, res) => {
 
     res.status(401).json({ message: 'Invalid Credentials '});
 })
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
